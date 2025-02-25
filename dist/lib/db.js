b91481ac1ffbf0db4699097ca262de2614b96620
Alan-denis <alan.denis@efrei.net>
2025-02-25 15:30:00 +0100

    added ENV vars

--- dist/lib/db.js
+++ dist/lib/db.js
@@ -4,8 +4,8 @@ const pg_1 = require("pg");
 const pool = new pg_1.Pool({
     host: process.env.POSTGRES_HOST || 'localhost',
     port: parseInt(process.env.POSTGRES_PORT || '5432'),
-    database: process.env.POSTGRES_DB || 'library',
-    user: process.env.POSTGRES_USER || 'libraryuser',
-    password: process.env.POSTGRES_PASSWORD || 'librarypass',
+    database: process.env.POSTGRES_DB || 'data',
+    user: process.env.POSTGRES_USER || 'user',
+    password: process.env.POSTGRES_PASSWORD || 'password',
 });
 exports.default = pool;
